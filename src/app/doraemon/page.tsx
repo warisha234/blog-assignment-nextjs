06f18732cbb39cb7643f7a4eb66ba299bc92f52a
warisha234 <asmaturab3@gmail.com>
2024-10-12 17:35:02 +0500

    Updated files as per changes

--- src/app/doraemon/page.tsx
+++ src/app/doraemon/page.tsx
@@ -6,7 +6,7 @@ export default function Blog() {
             <h2>Doraemon</h2>
             <Image src={Doraemon} alt="Doraemon" />
             <p>
-                "Doraemon" is a beloved animated series that has captured the hearts of audiences worldwide since its inception. The story revolves around a robotic cat named Doraemon, who is sent back in time from the 22nd century to help a clumsy and often unlucky boy named Nobita Nobi.
+                Doraemon is a beloved animated series that has captured the hearts of audiences worldwide since its inception. The story revolves around a robotic cat named Doraemon, who is sent back in time from the 22nd century to help a clumsy and often unlucky boy named Nobita Nobi.
 
                 Doraemon possesses a pocket from which he can pull out an array of futuristic gadgets and tools designed to solve problems and enhance Nobita's life. These gadgets range from time machines to gadgets that can alter reality, each introducing a new adventure and often leading to unexpected consequences. Despite his good intentions, Nobita's impulsive nature and laziness frequently lead to comical mishaps.
 
@@ -14,7 +14,7 @@ export default function Blog() {
 
                 The animation style, along with the memorable character designs, contributes to the show's charm. Supporting characters such as Shizuka, Gian, and Suneo add depth to the storyline and create a rich universe filled with diverse personalities.
 
-                "Doraemon" has enjoyed immense popularity in Japan and abroad, resulting in various adaptations, including movies, merchandise, and spin-offs. Its enduring appeal lies in its ability to entertain while imparting meaningful messages, making it a timeless classic in the realm of animation.
+                Doraemon has enjoyed immense popularity in Japan and abroad, resulting in various adaptations, including movies, merchandise, and spin-offs. Its enduring appeal lies in its ability to entertain while imparting meaningful messages, making it a timeless classic in the realm of animation.
             </p>
         </div>
 
--- src/app/miraculous/page.tsx
+++ src/app/miraculous/page.tsx
@@ -6,16 +6,16 @@ export default function Blog() {
             <h2>Miraculous</h2>
             <Image src={Miraculous} alt="miraculous" />
             <p>
-                "Miraculous: Tales of Ladybug & Cat Noir" has gained immense popularity since its debut, captivating audiences with its unique blend of action, romance, and comedy. The series revolves around the lives of two high school students, Marinette Dupain-Cheng and Adrien Agreste, who lead double lives as superheroes.
+                Miraculous Tales of Ladybug & Cat Noir has gained immense popularity since its debut, captivating audiences with its unique blend of action, romance, and comedy. The series revolves around the lives of two high school students, Marinette Dupain-Cheng and Adrien Agreste, who lead double lives as superheroes.
 
                 When danger arises in Paris, Marinette transforms into Ladybug, a brave and resourceful superhero equipped with magical powers granted by her Miraculous, a magical jewel. Similarly, Adrien becomes Cat Noir, a charming and agile hero, with his own set of powers. Together, they battle against the nefarious Hawk Moth, who uses his powers to turn ordinary citizens into supervillains known as Akumas.
 
-                The series highlights themes of friendship, teamwork, and self-discovery as Marinette and Adrien navigate their feelings for each other, unaware of each other's true identities. Their dynamic is filled with humor and tension, as they confront not only external threats but also the challenges of teenage life.
+                The series highlights themes of friendship, teamwork, and self-discovery as Marinette and Adrien navigate their feelings for each other, unaware of each others true identities. Their dynamic is filled with humor and tension, as they confront not only external threats but also the challenges of teenage life.
 
-                The animation style is vibrant and colorful, bringing to life the beautiful landscapes of Paris and the dynamic action sequences. The supporting characters, including Alya, Luka, and Master Fu, contribute to the story's richness and depth, each with their own unique qualities and story arcs.
-                "Miraculous" has also spawned a wide range of merchandise, video games, and even a musical. Its catchy theme song and engaging storylines have resonated with viewers, making it a beloved series for children and teens around the world.
+                The animation style is vibrant and colorful, bringing to life the beautiful landscapes of Paris and the dynamic action sequences. The supporting characters, including Alya, Luka, and Master Fu, contribute to the storys richness and depth, each with their own unique qualities and story arcs.
+                Miraculous has also spawned a wide range of merchandise, video games, and even a musical. Its catchy theme song and engaging storylines have resonated with viewers, making it a beloved series for children and teens around the world.
 
-                Overall, "Miraculous: Tales of Ladybug & Cat Noir" is a delightful series that combines superhero action with relatable themes of love and friendship, making it a standout in the realm of animated television.
+                Overall, "Miraculous: Tales of Ladybug & Cat Noir is a delightful series that combines superhero action with relatable themes of love and friendship, making it a standout in the realm of animated television.
             </p>
         </div>
     )
